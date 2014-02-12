172a057fa5ab296c64a3feb2510dea5660156d34
JagWire <babiuchr@missouri.edu>
2014-02-12 16:11:21 -0600

    Network function produces adjacency lists.

--- R/teva.networks.R
+++ R/teva.networks.R
@@ -2,7 +2,21 @@ teva.networks <-
 function(csvdata) {
 
  .teva <- .jnew("edu/missouri/teva/TEvA");
-  message("\nPress enter to begin processing your csv file");
-  readline("");
-  .jcall(.teva, "[S","networks", csvdata, hash.asCSV(defaults));
+  raws <- .jcall(.teva, "[S","networks", csvdata, hash.asCSV(defaults));
+
+
+  #get column names
+  cols <- names(read.csv(text=raws[1]));
+  
+  message(paste("Number of tables: ", length(raws)));
+  index <- 2;
+  output <- list(1);
+  while(index <= length(raws)) {
+     output[(index-1)] <- list(read.csv(text=raws[index], col.names=cols,header=FALSE));
+     index <- index + 1;
+    
+
+  }
+  return(output);
 }
+
